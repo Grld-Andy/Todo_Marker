54102d856b742bd5df8796e0acf2898aaec65142
Andy Ansong <andyansong82@gmail.com>
2025-10-03 20:46:24 +0000

    create onboarding 1

--- lib/features/onboarding/presentation/pages/intro_page.dart
+++ lib/features/onboarding/presentation/pages/intro_page.dart
@@ -5,6 +5,41 @@ class IntroPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Column(
+          spacing: 10,
+          children: [
+            Expanded(
+              child: Container(
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(8),
+                  border: Border.all(
+                    width: 1,
+                    color: Colors.black
+                  )
+                ),
+                child: Image.asset(
+                  'assets/images/TIP.png',
+                  fit: BoxFit.cover,
+                ),
+              )
+            ),
+            TextButton(
+              onPressed: (){},
+              style: TextButton.styleFrom(
+                backgroundColor: Colors.yellow[600],
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadiusGeometry.circular(5),
+                )
+              ),
+              child: Text("HI")
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
