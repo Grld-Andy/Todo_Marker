23755c4c27f5ad5369f9a60c4e5a07aa8be61caa
Andy Ansong <andyansong82@gmail.com>
2025-10-08 12:48:30 +0000

    create form 3

--- lib/features/onboarding/presentation/pages/form_3_page.dart
+++ lib/features/onboarding/presentation/pages/form_3_page.dart
@@ -8,8 +8,39 @@ class Form3Page extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        spacing: 10,
         children: [
-          H1Text(text: "Would you like to track tasks by difficulty")
+          H1Text(text: "Would you like to track tasks by difficulty"),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              IconButton(
+                onPressed: (){},
+                icon: Icon(Icons.check),
+                style: IconButton.styleFrom(
+                  backgroundColor: Colors.green[400],
+                  foregroundColor: Colors.green[700],
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadiusGeometry.circular(10)
+                  ),
+                  iconSize: 70
+                ),
+              ),
+              IconButton(
+                onPressed: (){},
+                icon: Icon(Icons.close),
+                style: IconButton.styleFrom(
+                  backgroundColor: Colors.red[400],
+                  foregroundColor: Colors.red[700],
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadiusGeometry.circular(10)
+                  ),
+                  iconSize: 70
+                )
+              ),
+            ],
+          )
         ],
       )
     );
