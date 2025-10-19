1fb02817dca0f7e29842ef1ad2ddad881bdf3577
Andy Ansong <andyansong82@gmail.com>
2025-10-19 00:59:47 +0000

    display tasks list

--- lib/features/home/presentation/pages/home_page.dart
+++ lib/features/home/presentation/pages/home_page.dart
@@ -3,7 +3,6 @@ import 'package:task_marker/features/home/Model/Task.dart';
 import 'package:task_marker/features/home/data/greetings.dart';
 import 'package:task_marker/features/home/presentation/widgets/profile_section.dart';
 import 'package:task_marker/features/home/presentation/widgets/search_section.dart';
-import 'package:task_marker/shared/widgets/h1_text.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -25,7 +24,49 @@ class _HomePageState extends State<HomePage> {
         spacing: 20.0,
         children: [
           ProfileSection(greetings: _greetings),
-          SearchSection(controller: _controller)
+          SearchSection(controller: _controller),
+          Expanded(
+            child: Container(
+              decoration: BoxDecoration(
+                border: Border.all(width: 2)
+              ),
+              child: ListView.builder(
+                itemCount: tasks.length,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    leading: FlutterLogo(size: 50),
+                    title: Text(tasks[index].title),
+                    subtitle: Text(tasks[index].description),
+                    trailing: Row(
+                      children: [
+                        IconButton(
+                          onPressed: (){},
+                          style: IconButton.styleFrom(
+                            backgroundColor: Colors.green
+                          ),
+                          icon: Icon(Icons.check)
+                        ),
+                        IconButton(
+                          onPressed: (){},
+                          style: IconButton.styleFrom(
+                            backgroundColor: Colors.amber
+                          ),
+                          icon: Icon(Icons.edit)
+                        ),
+                        IconButton(
+                          onPressed: (){},
+                          style: IconButton.styleFrom(
+                            backgroundColor: Colors.red
+                          ),
+                          icon: Icon(Icons.delete)
+                        ),
+                      ],
+                    ),
+                  );
+                }
+              ),
+            ),
+          )
         ],
       ),
     );
