d6460561185ef5f77c754c594ead0dd563a2ceb9
Andy Ansong <andyansong82@gmail.com>
2025-10-19 01:51:39 +0000

    enchance ui for tasks

--- lib/features/home/presentation/widgets/task_list.dart
+++ lib/features/home/presentation/widgets/task_list.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:task_marker/features/home/model/task.dart';
+import 'package:task_marker/features/home/presentation/widgets/task_button.dart';
 
 class TasksList extends StatelessWidget {
   const TasksList({
@@ -19,40 +20,63 @@ class TasksList extends StatelessWidget {
         child: ListView.builder(
           itemCount: tasks.length,
           itemBuilder: (context, index) {
-            return ListTile(
-              leading: FlutterLogo(size: 50),
-              title: Text(tasks[index].title),
-              subtitle: Text(
-                tasks[index].description,
-                maxLines: 1,
-                overflow: TextOverflow.ellipsis,
+            final task = tasks[index];
+            return Card(
+              elevation: 2,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(12)
               ),
-              trailing: Row(
-                mainAxisSize: MainAxisSize.min,
-                spacing: 5,
-                children: [
-                  IconButton(
-                    onPressed: (){},
-                    style: IconButton.styleFrom(
-                      backgroundColor: Colors.green
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+                child: Row(
+                  spacing: 5,
+                  children: [
+                    FlutterLogo(size: 50),
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            task.title
+                          ),
+                          Text(
+                            task.description,
+                            maxLines: 1,
+                            overflow: TextOverflow.ellipsis,
+                            style: TextStyle(
+                              color: Colors.grey[700]
+                            ),
+                          )
+                        ],
+                      )
                     ),
-                    icon: Icon(Icons.check)
-                  ),
-                  IconButton(
-                    onPressed: (){},
-                    style: IconButton.styleFrom(
-                      backgroundColor: Colors.amber
-                    ),
-                    icon: Icon(Icons.edit)
-                  ),
-                  IconButton(
-                    onPressed: (){},
-                    style: IconButton.styleFrom(
-                      backgroundColor: Colors.red
-                    ),
-                    icon: Icon(Icons.delete)
-                  ),
-                ],
+                    Row(
+                      spacing: 5,
+                      children: [
+                        TaskButton(
+                          icon: Icons.check,
+                          color: Colors.green,
+                          bgColor: const Color.fromRGBO(76, 175, 80, 0.2),
+                          onPressed: () {},
+                        ),
+                        const SizedBox(width: 6),
+                        TaskButton(
+                          icon: Icons.edit,
+                          color: Colors.amber,
+                          bgColor: const Color.fromRGBO(255, 193, 7, 0.2),
+                          onPressed: () {},
+                        ),
+                        const SizedBox(width: 6),
+                        TaskButton(
+                          icon: Icons.delete,
+                          color: Colors.red,
+                          bgColor: const Color.fromRGBO(244, 67, 54, 0.2),
+                          onPressed: () {},
+                        ),
+                      ],
+                    )
+                  ],
+                ),
               ),
             );
           }
