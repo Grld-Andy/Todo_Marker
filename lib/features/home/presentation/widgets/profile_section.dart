a342fd6308eb39c49026e19e91fd116bb64b582f
Andy Ansong <andyansong82@gmail.com>
2025-10-20 02:30:00 +0000

    redesign profile section

--- lib/features/home/presentation/widgets/profile_section.dart
+++ lib/features/home/presentation/widgets/profile_section.dart
@@ -14,21 +14,37 @@ class ProfileSection extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: [
-        Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            H1Text(text: "$_greetings, username"),
-            Text("You have ${tasks.length} tasks today")
-          ],
-        ),
-        CircleAvatar(
-          radius: 20,
-          backgroundImage: AssetImage('assets/images/TIP.png'),
-        )
-      ],
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.amber[100], // soft background
+        borderRadius: BorderRadius.circular(16),
+      ),
+      padding: const EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              H1Text(text: "$_greetings, username"),
+              Text(
+                "You have ${tasks.length} tasks today",
+                style: TextStyle(
+                  color: Colors.grey[800]
+                ),
+              )
+            ],
+          ),
+          CircleAvatar(
+            radius: 24,
+            backgroundColor: Colors.amber[500],
+            child: CircleAvatar(
+              radius: 20,
+              backgroundImage: AssetImage('assets/images/TIP.png'),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
